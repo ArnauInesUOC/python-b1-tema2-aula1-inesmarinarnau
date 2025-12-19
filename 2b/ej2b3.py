f7592908840e5ba0b4d0fd5aeab790f8245ee769
ArnauInesUOC <ainesm@uoc.edu>
2025-12-19 13:42:16 +0100

    Ejercicios Tema 2 realizados y corregidos con tests satisfactoriamente

--- 2b/ej2b3.py
+++ 2b/ej2b3.py
@@ -20,37 +20,13 @@ Ejemplo:
 
     Salida:
     742.5
-
-
-Enunciat:
-Implementa una funció 'triangle_area_calculate', que rebi dos paràmetres,
-que corresponen a l'alçada i la base d'un triangle i que han de
-ser números positius. Aquests paràmetres han de ser nomenats correctament,
-considerant les bones pràctiques de programació PEP8.
-La funció ha de retornar el càlcul de l'àrea d'un triangle mitjançant les
-dades introduïdes, addicionalment, el codi ha de tenir comentaris de manera
-que es vagi explicant el procediment.
-
-Paràmetres:
-Són dos paràmetres, que corresponen a l'alçada i la base de
-un triangle i que han de ser números positius. S'han de crear correctament
-utilitzant les bones pràctiques de programació PEP8.
-
-
-Exemple:
-     Entrada:
-     triangle_area_calculate(33, 45)
-
-     Sortida:
-     742.5
-
 """
 
 
-def triangle_area_calculate(
-    base, height):
-    # Write here your code
-    pass
+def triangle_area_calculate(base, height):
+    if base <= 0 or height <= 0:
+        raise ValueError("Number must be greater than 0.")
+    return (base * height) / 2
 
 
 # Si quieres probar tu código, descomenta las siguientes líneas y ejecuta
@@ -59,4 +35,4 @@ def triangle_area_calculate(
 # Si vols provar el teu codi, descomenta les línies següents i executa
 # l'scrip
 
-# print(triangle_area_calculate(33, 45))
+print(triangle_area_calculate(33, 45))
--- 2d/ej2d1.py
+++ 2d/ej2d1.py
@@ -22,37 +22,21 @@ Ejemplo:
     Entrada: 'abc'
     Salida: TypeError
 
-Enunciat:
-Implementa una funció 'convert_kg_to_lb' que rebi com a paràmetre un valor
-numèric anomenat 'kg' que correspon al valor que es vol convertir de
-quilograms a lliures.
-
-El valor introduït no pot ser menor o igual que '0' si s'introdueix un valor
-menor o igual a '0' s'ha de crear un ValueError. El valor introduït ha de ser de
-tipus numèric de manera que si s'introdueix un altre valor que no sigui numèric s'haurà
-de crear un TypeError.
-
-Paràmetres:
-kg = Valor numèric que representa els quilograms per convertir lliures.
-
-Exemple:
-     Entrada: 50
-     Sortida: 110.23
-
-     Entrada: 0
-     Sortida: ValueError
-
-     Entrada: 'abc'
-     Sortida: TypeError
 
 """
 
 
 def kg_to_lb(kg):
-    # Write here your code
-    pass
+    try:
+        if kg <= 0:
+            raise ValueError("Must be greater than 0.")
+        else:
+            lb = kg * 2.20462
+            return round(lb, 2)
+    except TypeError:
+        raise TypeError("Must be a number.")
 
 
 # Si quieres probar tu código, descomenta las siguientes líneas y ejecuta el script
 # Si vols provar el teu codi, descomenta les línies següents i executa l'script
-#print(kg_to_lb(50))
+print(kg_to_lb(50))
